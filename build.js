7a33c96d7dd3c092de9f0674a8eaac90f2ee2a1d
shen.shuaijia <shen.shuaijia@sfit.shfe.com.cn>
2020-07-09 11:58:17 +0800

    new version

--- build.js
+++
@@ -1,9 +0,0 @@
-const buidMyPackage = require('build-my-package')
-const path = require('path')
-
-const { buildCommonjs } = buidMyPackage
-
-buildCommonjs({
-  entry: path.join(__dirname, './src'),
-  language: 'typescript'
-})
